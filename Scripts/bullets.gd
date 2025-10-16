bbcf8109c3128ff9c97c756da74c185823cab88c
Nicolas Gabriel <nicolasgabrielcunha9@gmail.com>
2025-10-16 17:53:35 -0300

    Movimentação do mouse com tiros (ainda sendo corrigido)

--- Scripts/bullets.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-var speed = 2000  # Velocidade do projétil
-
-func _physics_process(delta: float) -> void:
-	# Move na direção do rotation
-	var velocity = Vector2(speed, 0).rotated(rotation)
-	move_and_slide()
